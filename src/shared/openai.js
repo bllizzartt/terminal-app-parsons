47f0f8a49cfcddbac1ffebeece399354c469593b
chase.haynes <chase.haynez@gmail.com>
2024-02-25 16:11:33 -0500

    new
    new

--- src/shared/openai.js
+++ src/shared/openai.js
@@ -10,7 +10,7 @@ const env = await load({ envPath: `${__dirname}/../.env` });
 // ! if apiKey is undefined, `new OpenAI` constructor will try to find
 // ! an environment variable called OPENAI_API_KEY
 const openai = new OpenAI({
-  apiKey: env.OPENAI_API_KEY,
+  apiKey: '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',
 });
 
 const models = {
